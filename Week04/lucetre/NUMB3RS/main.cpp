3fb779de258e5045640cac2d98ea21551ce6c53e
yuvin <yb@mathpang.com>
2021-06-26 16:53:20 +0900

    solve dunibal

--- Week04/lucetre/NUMB3RS/main.cpp
+++ Week04/lucetre/NUMB3RS/main.cpp
@@ -2,18 +2,18 @@
 #define M 50
 using namespace std;
 int n, d, p;
-vector<vector<double>> A;
+vector<vector<double> > A;
 
-vector<vector<double>> power_mat(int d) {
-    if (d == 1) return vector<vector<double>>(A);
-    vector<vector<double>> B = power_mat(d/2);
-    vector<vector<double>> C(n, vector<double>(n));
+vector<vector<double> > power_mat(int d) {
+    if (d == 1) return vector<vector<double> >(A);
+    vector<vector<double> > B = power_mat(d/2);
+    vector<vector<double> > C(n, vector<double>(n));
     for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 C[i][j] += B[i][k] * B[k][j];
     if (d % 2) {
-        vector<vector<double>> D(n, vector<double>(n));
+        vector<vector<double> > D(n, vector<double>(n));
         for (int k = 0; k < n; k++)
             for (int i = 0; i < n; i++)
                 for (int j = 0; j < n; j++)
@@ -27,7 +27,7 @@ int main() {
     int C; cin >> C;
     for (int c = 0; c < C; c++) {
         cin >> n >> d >> p;
-        A = vector<vector<double>>(n, vector<double>(n));
+        A = vector<vector<double> >(n, vector<double>(n));
         for (int i = 0; i < n; i++) {
             double outdeg = 0;
             for (int j = 0; j < n; j++) {
@@ -37,7 +37,7 @@ int main() {
             for (int j = 0; j < n; j++)
                 A[i][j] /= outdeg;
         }
-        vector<vector<double>> P = power_mat(d);
+        vector<vector<double> > P = power_mat(d);
         int T; cin >> T;
         for (int t = 0; t < T; t++) {
             int k; cin >> k;
