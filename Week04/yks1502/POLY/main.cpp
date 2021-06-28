c1e56b906dc016c7d9be7bc6b51935bc3b62d84f
yks1502 <516yks@naver.com>
2021-06-28 15:09:19 +0900

    solved week04 poly

--- Week04/yks1502/POLY/main.cpp
+++ Week04/yks1502/POLY/main.cpp
@@ -1,15 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cache[101];
+int cache[101][101];
+int q = 10000000;
+
+int poly_first(int n, int first) {
+  int& c = cache[n][first];
+  if (c != -1) return c;
+  if (n == 1 || n == first) return c = 1;
+
+  c = 0;
+  for (int i = 1; i <= n - first; i++) {
+    c += (first + i - 1) * poly_first(n - first, i);
+    c %= q;
+  }
+
+  return c;
+}
 
 int poly(int n) {
-  int& c = cache[n];
-  if (c != -1) return c;
-  if (n == 0) return c = 0;
-  if (n == 1) return c = 1;
-
-  
+  int res = 0;
+  for (int i = 1; i <= n; i++) {
+    res += poly_first(n, i);
+    res %= q;
+  }
+  return res;
 }
 
 int main() {
@@ -19,5 +34,6 @@ int main() {
 
   for (int i = 0; i < C; i++) {
     cin >> n;
+    cout << poly(n) << endl;
   }
 }
