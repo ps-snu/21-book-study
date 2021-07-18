5e317272fd3a9c5c1cf4605bdac45852ad00be25
yuvin <dbqls9713@naver.com>
2021-07-18 23:54:28 +0900

    solve dragon

--- Week05/yuvin/DRAGON/main.py
+++ Week05/yuvin/DRAGON/main.py
@@ -10,10 +10,29 @@ def transpose(dragon_curve_str:str) -> str:
         else:
             res_str += c
     return res_str
+
+def get_last_len(c: str, n: int) -> int:
+    if c == "X" or c == "Y":
+        return 3 * int(math.pow(2,n)) - 2
+    else:
+        return 1
+
 for i in range(test_case_num):
     generation_num, start_pos, length = tuple(map(int,input().rstrip().split(" ")))
+    dragon_curve = "FX"
+    #X,Y,F,+,-
+    cnt_sum = 0
+    for j in range(generation_num):
+        dragon_curve = transpose(dragon_curve)
+        c = dragon_curve[0]
+        k = 0
+        while cnt_sum + get_last_len(c,generation_num - j - 1) < start_pos:
+            k += 1
+            dragon_curve = dragon_curve[1:]
+            cnt_sum += get_last_len(c,generation_num - j - 1)
+            c = dragon_curve[0]
 
-    dragon_curve = (0,"FX")
-    res_str = dragon_curve[2]
-    for i in range(generation_num):
-        dragon_curve = (dragon_curve[0], transpose(res_str))
+        if len(dragon_curve) > length:
+            dragon_curve = dragon_curve[:length]
+
+    print(dragon_curve[start_pos-cnt_sum -1 : start_pos - cnt_sum + length - 1])
