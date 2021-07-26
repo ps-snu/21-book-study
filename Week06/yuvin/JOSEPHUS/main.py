866f1196f91d0777fbf60c48cee56f781800519c
yuvin <yb@mathpang.com>
2021-07-26 23:01:56 +0900

    solve josephus

--- Week06/yuvin/JOSEPHUS/main.py
+++ Week06/yuvin/JOSEPHUS/main.py
@@ -1,22 +1,21 @@
 def transpose(a,n,k):
     if a + k > n:
-        return (a + k + 1) % n
+        return (a + k) % n + 1
     else:
         return a + k
     
-# def josephus(n: int, k: int):
-#     if n == 2:
-#         return 1, 2
-#     else:
-#         a, b = josephus(n-1, k)
-#         return transpose(a,n,k), transpose(b,n,k)
+def josephus(n: int, k: int):
+    if n == 2:
+        return 1, 2
+    else:
+        a, b = josephus(n-1, k)
+        return transpose(a,n,k), transpose(b,n,k)
 
 res_list = [(1,2)]
 
 test_case_num = int(input().strip())
 for i in range(test_case_num):
     n, k = tuple(map(int,input().strip().split(" ")))
-    # print(*josephus(n,k))
     res_list = [(1,2)]
     for j in range(3,n+1):
         res_list.append((transpose(res_list[-1][0],j,k),transpose(res_list[-1][1],j,k)))
