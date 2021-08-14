aa3022c3c3f47b2902f4360ba1c2d96a2932b455
yks1502 <516yks@naver.com>
2021-08-14 14:57:44 +0900

    Solving Week09 HANOI4

---
+++ Week09/yks1502/HANOI4B/timeout.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int C, N, n, num;
+map<int, int> steps;
+
+int getState(int state, int no) {
+    return (state >> (2 * no)) & 3;
+}
+
+int setState(int state, int no, int value) {
+    return (state & ~(3 << (2 * no))) | (value << (2 * no));
+}
+
+vector<int> adjs(int state) {
+    vector<int> adj;
+
+    int top[4] = {-1, -1, -1, -1};
+    for (int i = N - 1; i >= 0; i--) {
+        top[getState(state, i)] = i;
+    }
+
+    // i -> j
+    for (int i = 0; i < 4; i++) {
+        if (top[i] == -1) continue;
+
+        for (int j = 0; j < 4; j++) {
+            if (top[j] == -1 || top[i] < top[j]) {
+                int v = setState(state, top[i], j);
+                adj.push_back(v);
+            }
+        }
+    }
+
+    return adj;
+}
+
+int sgn(int x) { if (!x) return 0; return x < 0 ? -1 : 1; }
+int inc(int x) { return x < 0 ? x - 1 : x + 1; }
+
+int bfs(int init, int final) {
+    if (init == final) return 0;
+    queue<int> q;
+    steps.clear();
+    
+    q.push(init);
+    q.push(final);
+    steps[init] = 1;
+    steps[final] = -1;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        vector<int> adj = adjs(u);
+
+        for (int i = 0; i < adj.size(); i++) {
+            int v = adj[i];
+
+            if (steps.count(v) == 0) {
+                steps[v] = inc(steps[u]);
+                q.push(v);
+            } else if (sgn(steps[u]) != sgn(steps[v])) {
+                return abs(steps[u]) + abs(steps[v]) - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    cin >> C;
+    for (int tc = 0; tc < C; tc++) {
+        cin >> N;
+        int init = 0;
+        int final = 0;
+
+        for (int i = 0; i < 4; i++) {
+            cin >> n;
+            for (int j = 0; j < n; j++) {
+                cin >> num;
+                init = setState(init, num - 1, i);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            final = setState(final, i, 3);
+        }
+
+        int result = bfs(init, final);
+        cout << result << endl;
+    }
+}
